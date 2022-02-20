4aebee6add85051bef79a92628a147232b32dbb9
Kelelchi Nwandu <nwandukelechi@gmail.com>
2022-02-20 13:27:32 +0100

    update

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Icon} from '@iconify/react';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 
 function Navigation() {
     return (
         <div className='row navigation g-0'>
             <div className='navigation--left col-6'>
-                <div className='navigation--body--1 col-lg-4 col-sm-4'>
+                <div className='navigation--body--1'>
                     Nearest rides
                 </div>
                 <div className='navigation--body--2'>
@@ -17,12 +18,23 @@ function Navigation() {
                 </div>
             </div>
             <div className='navigation--right col-1'>
-                <Icon icon="bi:filter-left" />
-                <p className='nav--right--text'>Filter</p>
+                <Dropdown>
+                    <Dropdown.Toggle className="filter--button" id="dropdown-basic">
+                        <Icon icon="bi:filter-left" width="25" height="25"/>
+                        Filter
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu className="dropdown--bg">
+                        <Dropdown.Item className="dropdown--bg">Filters</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item href="#/action-2" className="dropdown--bg">State</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3" className="dropdown--bg">City</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
         </div>
     );
-  }
+    }
 
 
 export default Navigation;
